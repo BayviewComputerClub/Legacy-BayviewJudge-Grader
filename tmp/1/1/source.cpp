1351747d090470cf2fd5341733d5450991816032
Seshan Ravikumar <seshan10@me.com>
2019-05-01 12:19:18 -0400

    Auto stash before merge of "master" and "origin/master"

--- tmp/1/1/source.cpp
+++ tmp/1/1/source.cpp
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 #include<iostream>
 using namespace std;
 int main(){
@@ -7,4 +8,15 @@ int main(){
     int c = a+b;
     cout<<c<<endl;
     return 0;
+=======
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a;
+    cin>>b;
+    int c = a+b;
+    cout<<c<<endl;
+    return 0;
+>>>>>>> Stashed changes
 }
